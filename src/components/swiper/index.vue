da7da86c57b037481d8f829a5a08165ca6947536
jinliangmei <jinliangmei@hengtiansoft.com>
2021-05-18 17:13:01 +0800

    swiper

--- src/components/swiper/index.vue
+++ src/components/swiper/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <!-- <swiper :slides-per-view="3" :space-between="50">
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-    </swiper> -->
+  <div class="swiper-bg">
+    <div class="swiper-box">
+      <swiper ref="mySwiper" :options="swiperOptions">
+        <swiper-slide v-for="(item,index) in bannerList" :key="index">
+          <!-- <img :src="item.imageUrl"> -->
+          {{item.typeTitle}}
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+    ``</swiper>
+    </div>
   </div>
 </template>
 <script>
-// import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
 import { mixins } from './mixins'
 export default {
   mixins: [mixins],
@@ -18,9 +23,50 @@ export default {
       required: true
     },
   },
-  // components: {
-  //   Swiper,
-  //   SwiperSlide,
-  // },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      swiperOptions: {
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false
+        },
+        loop: true,
+        speed: 1000,
+        // 显示分页
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true //允许分页点击跳转
+        },
+        // 设置点击箭头
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      }
+    }
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.$swiper
+    }
+  },
+  mounted() {
+    console.log(this.bannerList);
+    console.log('Current Swiper instance object', this.swiper)
+    this.swiper.slideTo(3, 1000, false)
+  }
+  // directives: {
+  //   swiper: directive
+  // }
 }
 </script>
+<style lang="scss" scoped>
+.swiper-box {
+  width: 1100px;
+  margin: 0 auto;
+}
+</style>
