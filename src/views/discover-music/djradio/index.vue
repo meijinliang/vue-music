e3d04a674155d5009443e020be4b855a033a286d
jinliangmei <jinliangmei@hengtiansoft.com>
2021-07-21 17:39:06 +0800

    对接排行榜详情

---
+++ src/views/discover-music/djradio/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'djRadio',
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/discover-music/toplist/index.vue
+++ src/views/discover-music/toplist/index.vue
@@ -1,7 +1,9 @@
 <template>
+<!-- 发现音乐 - 排行榜 -->
   <div class="basic-container toplist">
+    <!-- 左边榜单列表 -->
     <div class="toplist-left">
-      <div class="g-sd">
+      <!-- <div class="g-sd">
         <div>
           <h2 class="g-sd-1">云音乐特色榜</h2>
           <list-item :data-source="musicFeaturelist" :current-list-id="currentListId" @choose="changeCurrentListId" />
@@ -10,8 +12,9 @@
           <h2 class="g-sd-1 mt16">全球媒体榜</h2>
           <list-item :data-source="globalMediaList" :current-list-id="currentListId" @choose="changeCurrentListId" />
         </div>
-      </div>
+      </div> -->
     </div>
+    <!-- 右边榜单详情 -->
     <div class="toplist-right">
       <div class="toplist-wrap1">
         <div class="cover-bg cover text-center">
@@ -21,12 +24,58 @@
           <h3>{{ playList.name }}</h3>
           <div>
             <i></i>
-            <span>最近更新：{{ new Date(playList.trackUpdateTime) | parseTime('{y}年{m}月{d}日')}}</span>
+            <span>最近更新：{{ updateTime }}</span>
           </div>
         </div>
       </div>
       <div class="toplist-wrap2">
-
+        <!-- 表格顶部信息 -->
+        <el-row type="flex" justify="space-between">
+          <el-col>
+            <h2 class="inline-block mr16">歌曲列表</h2>
+            <span v-if="playList.tracks"> {{ playList.tracks.length }}首歌</span>
+          </el-col>
+          <el-col align="right">
+            <div class="play-count">
+              播放：<span class="text-red">{{ playList.playCount}}</span> 次
+            </div>
+          </el-col>
+        </el-row>
+        <!-- 歌单列表 -->
+         <el-table 
+          :data="tableData"
+          v-loading="loading"
+          stripe
+          :header-row-class-name="headerClassName"
+          >
+          <el-table-column type="index" width="100">
+            <template v-slot="{row, $index}">
+              <span>{{ $index + 1 }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="标题">
+            <template v-slot="{row, $index}">
+              <div>
+                <img v-if="$index < 3" :src="row.al.picUrl">
+                <span>{{ row.name}}</span>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="时长" width="80">
+            <template v-slot="{row}">
+              <span> {{ formatTime(row.dt) }} </span>
+            </template>
+          </el-table-column>
+          <el-table-column label="歌手" width="180">
+            <template v-slot="{row}">
+              <div class="ellipsis">
+                <span v-for="(item, index) in row.ar" :key="item.id">{{ item.name }}
+                  <span v-if="index !== row.ar.length - 1"> / </span>
+                </span>
+              </div>
+            </template>
+          </el-table-column>
+        </el-table>
       </div>
       <!-- <el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop> -->
     </div>
@@ -34,16 +83,13 @@
 </template>
 
 <script>
-import ListItem from './ListItem.vue'
+// import ListItem from './ListItem.vue'
 import { playListDetail } from '@/api/index'
 import { parseTime } from '@/utils/index'
 export default {
   name: 'DiscoverToplist',
-  filters: {
-    parseTime
-  },
   components: {
-    ListItem
+    // ListItem
   },
   data () {
     return {
@@ -52,7 +98,13 @@ export default {
       // 当前选中的榜单
       currentListId: -1,
       // 右边对应榜单的详情
-      playList: {}
+      playList: {},
+      // 榜单最近更新时间
+      updateTime: '',
+      // 加载表格列表
+      loading: false,
+      // 表格数据
+      tableData: []
     }
   },
   computed: {
@@ -90,10 +142,24 @@ export default {
     },
     // 获取榜单详情
     getListDetail () {
+      this.loading = true
       playListDetail(this.currentListId).then(res => {
         this.playList = res?.playlist
-        console.log(this.playList);
+        this.tableData = res?.playlist.tracks
+        this.updateTime = parseTime(new Date(this.playList.trackUpdateTime), '{y}年{m}月{d}日')
+      }).finally(() => {
+        this.loading = false
       })
+    },
+    // 总毫秒处理成分秒
+    formatTime(val) {
+      let m,s
+      m = Math.floor(val / 1000 / 60) < 10 ? '0' + Math.floor(val / 1000 / 60) : Math.floor(val / 1000 / 60)
+      s= val % 60 < 10 ? '0' + val % 60 : val % 60
+      return m + ':'+ s
+    },
+    headerClassName() {
+      return 'header-class'
     }
   }
 }
@@ -114,8 +180,9 @@ export default {
   }
   &-right {
     flex: 1;
+    border-right: 1px solid #ccc;
     .toplist-wrap1 {
-      padding: 40px;
+      padding: 24px;
       display: flex;
       .cover {
         width: 158px;
@@ -129,6 +196,22 @@ export default {
         }
       }
     }
+    .toplist-wrap2 {
+      padding: 0 24px;
+      .play-count {
+        line-height: 24px;
+      }
+    }
   }
 }
+// .el-table__header-wrapper {
+//   /deep/ .header-class {
+//     border: 1px solid #ccc;
+//   }
+// }
+.el-table {
+  border: 1px solid #ccc;
+  border-top: 2px solid #c20c0c;
+}
+   
 </style>
